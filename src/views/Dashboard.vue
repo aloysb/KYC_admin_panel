76b73599b7b1ad349dfe9d47316e34b6fa027aac
Aloys Berger <aloysberger@gmail.com>
2020-05-02 13:26:23 +0200

    Get all user on button click (will have to be fetch on load)

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,23 +2,38 @@
   <div class="about">
     <p>Connection succesful!</p>
     <h1>This is the dashboard</h1>
-    <button v-on:click="getAllUsers(token)">Get all users</button>
+    <v-btn v-on:click="getAllUsers()">Get all users</v-btn>
   </div>
 </template>
 
 
 <script>
+  import axios from 'axios';
+
   export default{
     name: 'Dashboard',
     data(){
       return{
+        //Config for API request.
+        config: {headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${this.token}`
+          }
+        },
+        users:[]
       }
     },
     props:['token'],
     methods:{
-      getAllUsers(headers){
-        console.log('here');
-        console.log(headers);
+      getAllUsers(){
+        console.log(this.token);
+        console.log(this.config);
+        axios.get('https://kyc.to.wtf/api/admin/users',this.config)
+        .then(res => {
+          if (res.status != 200) throw Error;
+          this.users = res.data.items;
+        })
+        .catch(err => console.log(err));
       }
     }
   }
