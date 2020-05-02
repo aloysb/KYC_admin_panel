5d9fa21da29c923b7a1fee1513e89b7964ce22e4
Aloys Berger <aloysberger@gmail.com>
2020-05-02 16:05:06 +0200

    Add data table of all users

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,28 +2,28 @@
     <div class="about">
         <p>Connection succesful!</p>
         <h1>This is the dashboard</h1>
-        <v-row justify= 'center'>
-        <v-dialog persistent v-model='newUserForm' max-width='600px'>
-
-            <template v-slot:activator="{ on }">
-                <v-btn color="primary" dark v-on="on">+ Create new user</v-btn>
-            </template>
-                      <v-card>
-                <v-form class='elevation-7 px-12 py-6'>
-                    <v-text-field v-model='newUser.first_name' placeholder='First name'></v-text-field>
-                    <v-text-field v-model='newUser.middle_name' placeholder='Middle name'></v-text-field>
-                    <v-text-field v-model='newUser.last_name' placeholder='Last name'></v-text-field>
-                    <v-text-field v-model='newUser.email' placeholder='Email'></v-text-field>
-                            <v-card-actions>
-          <v-spacer></v-spacer>
-                    <v-btn color="error darken-1" text @click="newUserForm = false">Close</v-btn>
-                    <v-btn color="primary darken-1" text @click="createNewUser">Save</v-btn>
-        </v-card-actions>
-
-                </v-form>
-              </v-card>
-        </v-dialog>
-      </v-row>
+        <v-row justify='center'>
+            <v-dialog persistent v-model='newUserForm' max-width='600px'>
+                <template v-slot:activator="{ on }">
+                    <v-btn color="primary" dark v-on="on">+ Create new user</v-btn>
+                </template>
+                <v-card>
+                    <v-form class='elevation-7 px-12 py-6'>
+                        <v-text-field v-model='newUser.first_name' placeholder='First name'></v-text-field>
+                        <v-text-field v-model='newUser.middle_name' placeholder='Middle name'></v-text-field>
+                        <v-text-field v-model='newUser.last_name' placeholder='Last name'></v-text-field>
+                        <v-text-field v-model='newUser.email' placeholder='Email'></v-text-field>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="error darken-1" text @click="newUserForm = false">Close</v-btn>
+                            <v-btn color="primary darken-1" text @click="createNewUser">Save</v-btn>
+                        </v-card-actions>
+                    </v-form>
+                </v-card>
+            </v-dialog>
+        </v-row>
+        <v-data-table :items='this.users' :headers='this.headers'>
+        </v-data-table>
     </div>
 </template>
 <script>
@@ -48,6 +48,24 @@ export default {
                 "last_name": ""
             },
             newUserForm: false,
+            headers: [
+              {
+                text: 'First Name',
+                value: 'first_name'
+              },
+              {
+                text: 'Last Name',
+                value: 'last_name'
+              },
+
+              {
+                text: 'Email',
+                value: 'email'
+              },{
+                text: 'Status',
+                passed: 'passed'
+              }
+            ]
         }
     },
     props: ['token'],
