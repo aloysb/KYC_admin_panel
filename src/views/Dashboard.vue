dc79ea616fc795cfc78d6296ad0f9f0d99826772
Aloys Berger <aloysberger@gmail.com>
2020-05-02 13:33:29 +0200

    Removed console.log development

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,6 +2,7 @@
   <div class="about">
     <p>Connection succesful!</p>
     <h1>This is the dashboard</h1>
+    <v-btn v-on:click="createNewUser()"></v-btn>
   </div>
 </template>
 
@@ -25,14 +26,15 @@
     props:['token'],
     methods:{
       getAllUsers(){
-        console.log(this.token);
-        console.log(this.config);
         axios.get('https://kyc.to.wtf/api/admin/users',this.config)
         .then(res => {
           if (res.status != 200) throw Error;
           this.users = res.data.items;
         })
         .catch(err => console.log(err));
+      },
+      createNewUser(){
+
       }
     },
     created(){
