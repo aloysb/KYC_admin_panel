c87d6d73ae097e792cfa81b0f82ef397d6da930f
Aloys Berger <aloysberger@gmail.com>
2020-05-03 11:04:01 +0200

    git related commit

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,87 +1,79 @@
 <template>
-
-    <div class = "dashboard__wrapper">
-      <!-- HEADER -->
-        <v-btn v-on:click="logout" class = 'header__logout' color='error' >Logout</v-btn>
-      <v-row>
-      <v-col cols = '12' class = 'mx-auto ma-4 d-flex align-center justify-center pa-12'>
-        <v-card min-width = '100%' max-width='1200px' class='mx-auto pl-12 pr-12 pt-6 elevation-10 align-self-stretch'>
-            <h1 class = 'display-1 align-left light-blue darken-2 pa-2 white--text mb-4'>KYC User Datable</h1>
-            <v-row class='d-flex space-between'>
-              <v-col cols = '3' class='text-left'>
-
-                <!-- ADD NEW USER FORM -->
-                <v-dialog persistent v-model='newUserForm' max-width='600px'>
-                    <template v-slot:activator="{ on }">
-                        <v-btn color="primary" dark v-on="on" x-large>+ Create new user</v-btn>
-                    </template>
-                    <v-card>
-                        <v-form class='elevation-7 px-12 py-6'>
-                            <v-text-field v-model='newUser.first_name' placeholder='First name'></v-text-field>
-                            <v-text-field v-model='newUser.middle_name' placeholder='Middle name'></v-text-field>
-                            <v-text-field v-model='newUser.last_name' placeholder='Last name'></v-text-field>
-                            <v-text-field v-model='newUser.email' placeholder='Email'></v-text-field>
-                            <v-card-actions>
-                                <v-spacer></v-spacer>
-                                <v-btn color="error darken-1" text @click="newUserForm = false">Close</v-btn>
-                                <v-btn color="primary darken-1" text @click="createNewUser">Save</v-btn>
-                            </v-card-actions>
-                        </v-form>
-                    </v-card>
-                </v-dialog>
-              </v-col>
-              <v-col offset = '3' cols= '6'>
-
-                <!-- SEARCH FIELD -->
-                <v-text-field v-model="search" append-icon="mdi-magnify" label="Search user" single-line hide-details></v-text-field>
-              </v-col>
-            </v-row>
-
-            <!--start of data table--->
-            <v-data-table :items='this.users' :headers='this.dataHeaders' :search='this.search' loading=false loading-text="Loading... Please wait" sortBy="last_name">
-
-              <!-- Format last update date -->
-              <template v-slot:item.last_update="{ item }">
-                <p>{{formatDate(item.last_update)}}</p>
-              </template>
-
-              <!-- EDITABLE REJECTED REASON -->
-                <template v-slot:item.rejected_reason="props">
-                    <v-edit-dialog :return-value.sync="props.item.reject_reason" large persistent @save="save(props.item)" @cancel="cancel">
-                        <div>{{ props.item.rejected_reason }}</div>
-                        <template v-slot:input>
-                            <div class="mt-4 title">Enter reason for rejection</div>
+    <div class="dashboard__wrapper">
+        <!-- HEADER -->
+        <v-btn v-on:click="logout" class='header__logout' color='error'>Logout</v-btn>
+        <v-row>
+            <v-col cols='12' class='mx-auto ma-4 d-flex align-center justify-center pa-12'>
+                <v-card min-width='100%' max-width='1200px' class='mx-auto pl-12 pr-12 pt-6 elevation-10 align-self-stretch'>
+                    <h1 class='display-1 align-left light-blue darken-2 pa-2 white--text mb-4'>KYC User Datable</h1>
+                    <v-row class='d-flex space-between'>
+                        <v-col cols='3' class='text-left'>
+                            <!-- ADD NEW USER FORM -->
+                            <v-dialog persistent v-model='newUserForm' max-width='600px'>
+                                <template v-slot:activator="{ on }">
+                                    <v-btn color="primary" dark v-on="on" x-large>+ Create new user</v-btn>
+                                </template>
+                                <v-card>
+                                    <v-form class='elevation-7 px-12 py-6'>
+                                        <v-text-field v-model='newUser.first_name' placeholder='First name'></v-text-field>
+                                        <v-text-field v-model='newUser.middle_name' placeholder='Middle name'></v-text-field>
+                                        <v-text-field v-model='newUser.last_name' placeholder='Last name'></v-text-field>
+                                        <v-text-field v-model='newUser.email' placeholder='Email'></v-text-field>
+                                        <v-card-actions>
+                                            <v-spacer></v-spacer>
+                                            <v-btn color="error darken-1" text @click="newUserForm = false">Close</v-btn>
+                                            <v-btn color="primary darken-1" text @click="createNewUser">Save</v-btn>
+                                        </v-card-actions>
+                                    </v-form>
+                                </v-card>
+                            </v-dialog>
+                        </v-col>
+                        <v-col offset='3' cols='6'>
+                            <!-- SEARCH FIELD -->
+                            <v-text-field v-model="search" append-icon="mdi-magnify" label="Search user" single-line hide-details></v-text-field>
+                        </v-col>
+                    </v-row>
+                    <!--start of data table--->
+                    <v-data-table :items='this.users' :headers='this.dataHeaders' :search='this.search' loading=false loading-text="Loading... Please wait" sortBy="last_name">
+                        <!-- DATE FORMATING -->
+                        <template v-slot:item.last_update="{ item }">
+                            {{formatDate(item.last_update)}}
                         </template>
-                        <template v-slot:input>
-                            <v-text-field v-model="props.item.rejected_reason" label="Edit" single-line counter autofocus></v-text-field>
+                        <!-- EDITABLE REJECTED REASON -->
+                        <template v-slot:item.rejected_reason="props">
+                            <v-edit-dialog :return-value.sync="props.item.reject_reason" large persistent @save="save(props.item)" @cancel="cancel">
+                                <div>{{ props.item.rejected_reason }}</div>
+                                <template v-slot:input>
+                                    <div class="mt-4 title">Enter reason for rejection</div>
+                                </template>
+                                <template v-slot:input>
+                                    <v-text-field v-model="props.item.rejected_reason" label="Edit" single-line counter autofocus></v-text-field>
+                                </template>
+                            </v-edit-dialog>
                         </template>
-                    </v-edit-dialog>
-                </template>
-
-                <!-- STATUS COLOR -->
-                    <template v-slot:item.passed="{ item }">
-      <v-chip :color="getColor(item.passed)" dark>{{(item.passed == 0)? 'R':'A'}}</v-chip>
-    </template>
-
-              <!-- APPROVED OR REJECT BUTTON -->
-                <template v-slot:item.reject="{ item }">
-                    <v-icon large class="mr-2" color='green' @click="approveUser(item)" >
-                        mdi-account-check
-                    </v-icon>
-                </template>
-                <template v-slot:item.approve="{ item }">
-                    <v-icon  large color='red' @click="rejectUser(item)">
-                        mdi-account-cancel
-                    </v-icon>
-                </template>
-            </v-data-table>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-snackbar v-model="snack" :timeout="3000" :color="snackColor" top>
-      {{ snackText }}
-      <v-btn text @click="snack = false">Close</v-btn>
-    </v-snackbar>
+                        <!-- STATUS COLOR -->
+                        <template v-slot:item.passed="{ item }">
+                            <v-chip :color="getColor(item.passed)" dark>{{(item.passed == 0)? 'R':'A'}}</v-chip>
+                        </template>
+                        <!-- APPROVED OR REJECT BUTTON -->
+                        <template v-slot:item.reject="{ item }">
+                            <v-icon large class="mr-2" color='green' @click="approveUser(item)">
+                                mdi-account-check
+                            </v-icon>
+                        </template>
+                        <template v-slot:item.approve="{ item }">
+                            <v-icon large color='red' @click="rejectUser(item)">
+                                mdi-account-cancel
+                            </v-icon>
+                        </template>
+                    </v-data-table>
+                </v-card>
+            </v-col>
+        </v-row>
+        <v-snackbar v-model="snack" :timeout="3000" :color="snackColor" top>
+            {{ snackText }}
+            <v-btn text @click="snack = false">Close</v-btn>
+        </v-snackbar>
     </div>
 </template>
 <script>
@@ -124,8 +116,8 @@ export default {
                     text: 'Email',
                     value: 'email'
                 }, {
-                  text: 'Updated',
-                  value: 'last_update'
+                    text: 'Updated',
+                    value: 'last_update'
                 }, {
                     text: 'Status',
                     value: 'passed',
@@ -174,13 +166,13 @@ export default {
 
 
             axios.put('https://kyc.to.wtf/api/admin/kyc_status', approvedUser, this.config)
-            .then(() => {
-            this.snack = true
-            this.snackColor = 'success'
-            this.snackText = `${user.first_name} ${user.last_name} approved!`
-            })
-            .then(() => this.getAllUsers())
-            .catch(err => console.log(err))
+                .then(() => {
+                    this.snack = true
+                    this.snackColor = 'success'
+                    this.snackText = `${user.first_name} ${user.last_name} approved!`
+                })
+                .then(() => this.getAllUsers())
+                .catch(err => console.log(err))
         },
         rejectUser(user) {
             console.log(user.rejected_reason);
@@ -192,39 +184,39 @@ export default {
 
             axios.put('https://kyc.to.wtf/api/admin/kyc_status', rejectedUser, this.config).
             then(() => {
-               this.snack = true
-                this.snackColor = 'error'
-                this.snackText = `${user.first_name} ${user.last_name} rejected!`
-            }).then(() => this.getAllUsers())
-            .catch(err => console.log(err))
+                    this.snack = true
+                    this.snackColor = 'error'
+                    this.snackText = `${user.first_name} ${user.last_name} rejected!`
+                }).then(() => this.getAllUsers())
+                .catch(err => console.log(err))
         },
-        formatDate(date){
-          return moment(date).fromNow()
+        formatDate(date) {
+            return moment(date).fromNow()
         },
 
         save(user) {
             this.rejectUser(user).
             then(() => {
-              this.snack = true
-              this.snackColor = 'success'
-              this.snackText = 'Data saved'
-            }).then(()=> this.getAllUsers())
-                        .catch(err => console.log(err))
+                    this.snack = true
+                    this.snackColor = 'success'
+                    this.snackText = 'Data saved'
+                }).then(() => this.getAllUsers())
+                .catch(err => console.log(err))
 
         },
         cancel() {
-          this.snack = true;
-          this.snackColor = 'error';
-          this.snackText = 'Reason for rejection not saved.'
+            this.snack = true;
+            this.snackColor = 'warning';
+            this.snackText = 'Data not saved.'
         },
 
-        getColor(status){
-          return (status != 0)? "green":"red"
+        getColor(status) {
+            return (status != 0) ? "green" : "red"
         },
 
-        logout(){
-          this.$emit('logged-out');
-          this.$router.push('/');
+        logout() {
+            this.$emit('logged-out');
+            this.$router.push('/');
         }
 
     },
@@ -234,38 +226,41 @@ export default {
 }
 </script>
 <style sccoped>
-
-  .dashboard__wrapper{
+.dashboard__wrapper {
     min-height: 100vh;
     width: 100vw;
-    background: #136a8a;  /* fallback for old browsers */
-background: -webkit-linear-gradient(to right, #267871, #136a8a);  /* Chrome 10-25, Safari 5.1-6 */
-background: linear-gradient(to right, #267871, #136a8a); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    background: #136a8a;
+    /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #267871, #136a8a);
+    /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #267871, #136a8a);
+    /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
-  }
+}
 
 /*  .header{
     background: rgba(0, 172, 193,0.3)
   }*/
 
-  .header__logout{
+.header__logout {
     position: absolute;
     right: 0;
     top: 0;
     margin: 1em;
 }
 
-  /*Data table*/
-  table{
+/*Data table*/
+table {
     margin-top: 1em;
     border-radius: 10px;
     border: .5px solid #aaa;
-  }
+}
 
-  tr{
-        border-radius: 10px;
-  }
-  .v-data-table-header th{
+tr {
+    border-radius: 10px;
+}
+
+.v-data-table-header th {
     align-content: center;
     justify-content: center;
     font-size: .8rem;
@@ -273,21 +268,21 @@ background: linear-gradient(to right, #267871, #136a8a); /* W3C, IE 10+/ Edge, F
     padding: 0;
     border-top: .5px solid #aaa;
     border-bottom: 1px solid #aaa;
-  }
+}
 
-   .v-application--is-ltr .v-data-table th{
-          text-align: center !important;
-    }
+.v-application--is-ltr .v-data-table th {
+    text-align: center !important;
+}
 
-   .v-data-table-header th:first-child{
-            border-radius: 10px 0 0 0;
-        border-left: .5px solid #aaa;
+.v-data-table-header th:first-child {
+    border-radius: 10px 0 0 0;
+    border-left: .5px solid #aaa;
 
-   }
+}
 
-      .v-data-table-header th:last-child{
-        border-radius: 0 10px 0 0;
-        border-right: .5px solid #aaa;
+.v-data-table-header th:last-child {
+    border-radius: 0 10px 0 0;
+    border-right: .5px solid #aaa;
 
-   }
+}
 </style>
